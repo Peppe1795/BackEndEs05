e51e201d8ce463d0782bd3adb577984cccc5cdec
Peppe1795 <petrucci238@gmail.com>
2023-07-03 15:57:09 +0200

    Es02

--- src/Es02/App02.java
+++ src/Es02/App02.java
@@ -1,10 +1,29 @@
 package Es02;
 
+import java.util.Scanner;
+
 public class App02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		try {
+			System.out.println("Inserisci km percorsi");
+			double km = input.nextDouble();
+			input.nextLine();
+			System.out.println("Inserisci litri di carburante");
+			double litri = input.nextDouble();
+			input.nextLine();
+			if (litri <= 0) {
+				System.out.println("Attenzione inserisci numero maggiore di 0");
+			} else {
+				System.out.println("Risultato " + km / litri);
+			}
 
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			input.close();
+		}
 	}
 
 }
