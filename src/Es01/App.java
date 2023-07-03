f85ec58ae7638bd133cc749d861b4190d11eca81
Peppe1795 <petrucci238@gmail.com>
2023-07-03 17:52:22 +0200

    correzione es 01

--- src/Es01/App.java
+++ src/Es01/App.java
@@ -18,28 +18,32 @@ public class App {
 
 		int posizione;
 		int valore;
-
-		do {
-			System.out.println(Arrays.toString(arrayN));
-			System.out.println("Inserisci numero riferito alla posizione possibile da 1 a 5");
-			posizione = input.nextInt() - 1;
-			input.nextLine();
-			if (posizione < 0 || posizione > 4) {
-				System.out.println("posizione selezionata non valida");
-				break;
-			}
-			System.out.println("inserisci il valore da sostituire se inserici 0 il programma di interrompe");
-			valore = input.nextInt();
-			if (valore < 1 || valore > 10) {
-				System.out.println("Attenzione inserisci un numero da 1 a 10");
-			} else {
-				arrayN[posizione] = valore;
-				input.nextLine();
+		try {
+			do {
 				System.out.println(Arrays.toString(arrayN));
-			}
+				System.out.println("Inserisci numero riferito alla posizione possibile da 1 a 5");
+				posizione = input.nextInt() - 1;
+				input.nextLine();
+				if (posizione < 0 || posizione > 4) {
+					System.out.println("posizione selezionata non valida");
+					break;
+				}
+				System.out.println("inserisci il valore da sostituire se inserici 0 il programma di interrompe");
+				valore = input.nextInt();
+				if (valore < 1 || valore > 10) {
+					System.out.println("Attenzione inserisci un numero da 1 a 10");
+				} else {
+					arrayN[posizione] = valore;
+					input.nextLine();
+					System.out.println(Arrays.toString(arrayN));
+				}
 
-		} while (valore != 0);
-		System.out.println("Programma terminato");
+			} while (valore != 0);
+			System.out.println("Programma terminato");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 		input.close();
 	}
 
