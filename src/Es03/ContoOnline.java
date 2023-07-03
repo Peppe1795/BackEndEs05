65e90545e83994fd99d7622f08d7f7d4e2e4329f
Peppe1795 <petrucci238@gmail.com>
2023-07-03 16:30:56 +0200

    es 03 quasi finito

---
+++ src/Es03/ContoOnline.java
@@ -0,0 +1,22 @@
+package Es03;
+
+public class ContoOnline extends ContoCorrente {
+	double maxPrelievo;
+
+	ContoOnline(String titolare, double saldo, double maxP) {
+		super(titolare, saldo);
+		this.maxPrelievo = maxP;
+	}
+
+	void stampaSaldo() {
+
+		System.out.println("Titolare: " + titolare + " - Saldo: " + saldo + " - Num movimenti: " + nMovimenti
+				+ " - Massimo movimenti: " + maxMovimenti + " - Massimo prelievo possibile: " + maxPrelievo);
+	}
+
+	void preleva(double x) throws BancaException {
+		if (x > maxPrelievo) {
+			throw new BancaException("il prelievo non è disponibile");
+		}
+	}
+}
